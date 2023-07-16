dc747d16299e4e403207c4b89958982dfeffc15b
Diksha <diksha66sharma78@gmail.com>
2023-07-16 16:38:35 +0530

    multi value containeters

---
+++ Session2.py
@@ -0,0 +1,8 @@
+"""rage containers
+multi storage containers-> homogeneous and heterogeneous """
+# 1.tuples
+names="john","jenneie","jim","jack","kim"
+print(names)
+
+#List
+names=["john","jenneie","kim"]
