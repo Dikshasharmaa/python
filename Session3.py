c1e1079715eb827925929f391144421422d487af
Diksha <diksha66sharma78@gmail.com>
2023-08-10 15:53:33 +0530

    Arithemetic operators

---
+++ Session3.py
@@ -0,0 +1,21 @@
+# Controller
+# 1. Operators
+# Arithmetic operators(+, -, *, **, /, //, % )
+
+number1 = int(input("Enter the first number"))
+number2 = int(input("Enter the second nuber"))
+
+Sum = number1 + number2
+subtraction = number1 - number2
+Multiplicity = number1 * number2
+Exponential = number1 ** number2
+Divide = number1 / number2
+Floor_divide = number1 // number2
+remainder = number1 % number2
+print("Sum is:", Sum)
+print("subtraction is:", subtraction)
+print("Multiplicity is:", Multiplicity)
+print("Exponential is:", Exponential)
+print("Divide is:", Divide)
+print("Floor_divide is:", Floor_divide)
+print("remainder is:", remainder)
